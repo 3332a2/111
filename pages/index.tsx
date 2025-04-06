6266d1e06c17c69d65fa9961c0076c9f772d6629
3332a2 <as36708@naver.com>
2025-04-07 06:28:37 +0900

    replace with new site files

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "3rem", fontFamily: "sans-serif" }}>
-      <h1>欢迎来到账号租赁服务中心</h1>
-      <p>本网站支持 GPT、YouTube、Netflix 合租账号。</p>
-      <p>部署成功后，我们会继续新增你的网站介绍、套餐、语言切换功能等。</p>
-    </div>
-  );
-}
